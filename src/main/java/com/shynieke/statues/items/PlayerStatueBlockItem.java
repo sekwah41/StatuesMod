ba998c7d068b3dc85bc59747d57e92aad6bc1af6
Mrbysco <Mrbysco@users.noreply.github.com>
2024-05-05 22:14:51 +0200

    Update PlayerStatueBlockItem.java

--- src/main/java/com/shynieke/statues/items/PlayerStatueBlockItem.java
+++ src/main/java/com/shynieke/statues/items/PlayerStatueBlockItem.java
@@ -4,7 +4,12 @@ import com.shynieke.statues.client.render.PlayerBEWLR;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ResolvableProfile;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.SkullBlockEntity;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 
 import java.util.function.Consumer;
@@ -13,6 +18,23 @@ public class PlayerStatueBlockItem extends StatueBlockItem {
 
 	public PlayerStatueBlockItem(Block blockIn, Properties builder) {
 		super(blockIn, builder);
+	}
+
+	@Override
+	public Component getName(ItemStack pStack) {
+		ResolvableProfile resolvableprofile = pStack.get(DataComponents.PROFILE);
+		return (Component) (resolvableprofile != null && resolvableprofile.name().isPresent()
+				? Component.translatable(this.getDescriptionId() + ".named", resolvableprofile.name().get())
+				: super.getName(pStack));
+	}
+
+	@Override
+	public void verifyComponentsAfterLoad(ItemStack pStack) {
+		ResolvableProfile resolvableprofile = pStack.get(DataComponents.PROFILE);
+		if (resolvableprofile != null && !resolvableprofile.isResolved()) {
+			resolvableprofile.resolve()
+					.thenAcceptAsync(profile -> pStack.set(DataComponents.PROFILE, profile), SkullBlockEntity.CHECKED_MAIN_THREAD_EXECUTOR);
+		}
 	}
 
 	@Override
