f192e0bdfecab80446a577b02a6cac61cbe73060
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-07 22:18:02 +0200

    Update neoforge support

--- src/main/java/com/shynieke/statues/registry/StatueEntities.java
+++ src/main/java/com/shynieke/statues/registry/StatueEntities.java
@@ -5,11 +5,11 @@ import com.shynieke.statues.entity.StatueBatEntity;
 import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 public class StatueEntities {
-	public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
-		event.register(StatueRegistry.STATUE_BAT.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, StatueBatEntity::canSpawnHere, SpawnPlacementRegisterEvent.Operation.AND);
+	public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+		event.register(StatueRegistry.STATUE_BAT.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, StatueBatEntity::canSpawnHere, RegisterSpawnPlacementsEvent.Operation.AND);
 	}
 
 	public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
