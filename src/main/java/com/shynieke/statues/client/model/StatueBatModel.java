39480cb1eda964885666dc8e976fa77c038532a5
Mrbysco <Mrbysco@users.noreply.github.com>
2024-08-18 16:36:03 +0200

    Fixed statue bat model

--- src/main/java/com/shynieke/statues/client/model/StatueBatModel.java
+++ src/main/java/com/shynieke/statues/client/model/StatueBatModel.java
@@ -1,9 +1,10 @@
 package com.shynieke.statues.client.model;
 
 import com.shynieke.statues.entity.StatueBatEntity;
+import net.minecraft.client.animation.definitions.BatAnimation;
 import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.util.Mth;
+import net.minecraft.client.renderer.RenderType;
 
 /**
  * Because vanilla BatModel doesn't allow any other class than BatEntity
@@ -16,49 +17,40 @@ public class StatueBatModel extends HierarchicalModel<StatueBatEntity> {
 	private final ModelPart leftWing;
 	private final ModelPart rightWingTip;
 	private final ModelPart leftWingTip;
+	private final ModelPart feet;
 
-	public StatueBatModel(ModelPart modelPart) {
-		this.root = modelPart;
-		this.head = modelPart.getChild("head");
-		this.body = modelPart.getChild("body");
+	public StatueBatModel(ModelPart root) {
+		super(RenderType::entityCutout);
+		this.root = root;
+		this.body = root.getChild("body");
+		this.head = root.getChild("head");
 		this.rightWing = this.body.getChild("right_wing");
 		this.rightWingTip = this.rightWing.getChild("right_wing_tip");
 		this.leftWing = this.body.getChild("left_wing");
 		this.leftWingTip = this.leftWing.getChild("left_wing_tip");
+		this.feet = this.body.getChild("feet");
 	}
 
+	@Override
 	public ModelPart root() {
 		return this.root;
 	}
 
-	public void setupAnim(StatueBatEntity statueBat, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		if (statueBat.isResting()) {
-			this.head.xRot = headPitch * 0.017453292F;
-			this.head.yRot = 3.1415927F - netHeadYaw * 0.017453292F;
-			this.head.zRot = 3.1415927F;
-			this.head.setPos(0.0F, -2.0F, 0.0F);
-			this.rightWing.setPos(-3.0F, 0.0F, 3.0F);
-			this.leftWing.setPos(3.0F, 0.0F, 3.0F);
-			this.body.xRot = 3.1415927F;
-			this.rightWing.xRot = -0.15707964F;
-			this.rightWing.yRot = -1.2566371F;
-			this.rightWingTip.yRot = -1.7278761F;
-			this.leftWing.xRot = this.rightWing.xRot;
-			this.leftWing.yRot = -this.rightWing.yRot;
-			this.leftWingTip.yRot = -this.rightWingTip.yRot;
-		} else {
-			this.head.xRot = headPitch * 0.017453292F;
-			this.head.yRot = netHeadYaw * 0.017453292F;
-			this.head.zRot = 0.0F;
-			this.head.setPos(0.0F, 0.0F, 0.0F);
-			this.rightWing.setPos(0.0F, 0.0F, 0.0F);
-			this.leftWing.setPos(0.0F, 0.0F, 0.0F);
-			this.body.xRot = 0.7853982F + Mth.cos(ageInTicks * 0.1F) * 0.15F;
-			this.body.yRot = 0.0F;
-			this.rightWing.yRot = Mth.cos(ageInTicks * 74.48451F * 0.017453292F) * 3.1415927F * 0.25F;
-			this.leftWing.yRot = -this.rightWing.yRot;
-			this.rightWingTip.yRot = this.rightWing.yRot * 0.5F;
-			this.leftWingTip.yRot = -this.rightWing.yRot * 0.5F;
+	/**
+	 * Sets this entity's model rotation angles
+	 */
+	@Override
+	public void setupAnim(StatueBatEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		this.root().getAllParts().forEach(ModelPart::resetPose);
+		if (entity.isResting()) {
+			this.applyHeadRotation(netHeadYaw);
 		}
+
+		this.animate(entity.flyAnimationState, BatAnimation.BAT_FLYING, ageInTicks, 1.0F);
+		this.animate(entity.restAnimationState, BatAnimation.BAT_RESTING, ageInTicks, 1.0F);
+	}
+
+	private void applyHeadRotation(float headRotation) {
+		this.head.yRot = headRotation * (float) (Math.PI / 180.0);
 	}
 }
