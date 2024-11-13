7c50b92e9af3069a7f4b5b652dcadfac59d02ade
Mrbysco <Mrbysco@users.noreply.github.com>
2024-11-13 16:08:47 +0100

    Remove unused method

--- src/main/java/com/shynieke/statues/items/StatueCoreItem.java
+++ src/main/java/com/shynieke/statues/items/StatueCoreItem.java
@@ -1,19 +1,10 @@
 package com.shynieke.statues.items;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-
-import java.util.List;
 
 public class StatueCoreItem extends Item {
 
 	public StatueCoreItem(Item.Properties builder) {
 		super(builder);
-	}
-
-	@Override
-	public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components, TooltipFlag tooltipFlag) {
 	}
 }
